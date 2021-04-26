c984386082b5140eb85446a156dde726c5ea3657
iserb <iserb1@gmail.com>
2021-04-26 12:33:12 +0300

    Added pessimistic read lock for getting account

--- src/main/java/com/exampledemo/bankaccount/repository/BankAccountRepository.java
+++ src/main/java/com/exampledemo/bankaccount/repository/BankAccountRepository.java
@@ -2,10 +2,15 @@ package com.exampledemo.bankaccount.repository;
 
 import com.exampledemo.bankaccount.model.BankAccount;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Lock;
 import org.springframework.stereotype.Repository;
+
+import javax.persistence.LockModeType;
 
 @Repository
 public interface BankAccountRepository extends JpaRepository<BankAccount, Long> {
-
+    @Lock(LockModeType.PESSIMISTIC_READ)
+    @Override
+    BankAccount getOne(Long id);
 
 }
